693e61ba4a2dde4b3d42242821dff07fa952ea19
ycc4477 <ycc4477@gmail.com>
2020-05-14 01:23:33 +0900

    4장 스터디 자료

---
+++ cyc/Chapter4/4.5/cluster.js
@@ -0,0 +1,34 @@
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`마스터 프로세스 아이디: ${process.pid}`);
+
+  for (let i = 0; i < numCPUs; i += 1) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`${worker.process.pid}번 워커가 종료되었습니다.`);
+    cluster.fork();
+  })
+} else {
+
+  http.createServer((req, res) => {
+    res.write('<h1>Hello Node!</h1>');
+    res.end('<p>Hello Server!</p>');
+
+    setTimeout(() => {
+      process.exit(1);
+    }, 1000)
+
+
+  }).listen(8083, () => {
+    console.log('8083번 포트에서 서버 대기 중입니다!')
+  })
+
+  console.log(`${process.pid}번 워커가 실행되었슴.`);
+
+
+}
