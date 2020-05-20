dacef0ed1806fe4d583465ad88f3e0f04304516b
yunchangchoi <ycc4477@gmail.com>
2020-05-21 00:50:29 +0900

    Chapter 6 - 01'

---
+++ cyc/Chapter6/6.1/learn-express/routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+router.get('/flash', function(req,res){
+  req.session.message = 'Session Message';
+  req.flash('message', 'flash Message');
+  req.redirect('users/flash/result');
+})
+
+router.get('/flash/result', function(req,res) {
+  req.send(`${req.session.message} ${req.flash('message')}`)
+})
+
+module.exports = router;
