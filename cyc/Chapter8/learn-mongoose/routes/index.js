71a146f22acf092862335f4ba98e051de3ff1950
yunchangchoi <ycc4477@gmail.com>
2020-06-11 06:35:59 +0900

    Chapter08

---
+++ cyc/Chapter8/learn-mongoose/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var User = require('../schemas/user')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  User.find({})
+  .then((users) => {
+  res.render('mongoose',{users});
+  })
+  .catch((err)=>{
+    console.error(err);
+    next(err);
+  })
+  
+});
+
+module.exports = router;
