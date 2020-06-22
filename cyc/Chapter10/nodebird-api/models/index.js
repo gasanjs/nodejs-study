a61a6ea9402eb97bc8aad389758f036b8a4fd00f
ycc4477 <ycc4477@gmail.com>
2020-06-23 00:04:08 +0900

    10.2

---
+++ cyc/Chapter10/nodebird-api/models/index.js
@@ -0,0 +1,51 @@
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database, config.username, config.password, config,
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+//Model
+db.User = require('./user')(sequelize, Sequelize);
+db.Post = require('./post')(sequelize, Sequelize);
+db.Hashtag = require('./hashtag')(sequelize, Sequelize);
+db.Domain =  require('./domain')(sequelize, Sequelize);
+
+//
+db.User.hasMany(db.Post);
+db.Post.belongsTo(db.User);
+db.Post.belongsToMany(db.Hashtag, {through: 'PostHashtag'});
+db.Hashtag.belongsToMany(db.Post, {through: 'PostHashtag'});
+db.User.belongsToMany(db.User, {
+  foreignKey: 'followingId',
+  as: 'Followers',
+  through: 'Follow',
+});
+db.User.belongsToMany(db.User, {
+  foreignKey: 'followerId',
+  as: 'Followings',
+  through: 'Follow',
+});
+
+//좋아요
+db.Post.belongsToMany(db.User, {
+  foreignKey: 'postId',
+  as: 'LikeUsers',
+  through: 'PostLike'
+});
+db.User.belongsToMany(db.Post, {
+  foreignKey: 'userId',
+  as: 'LikePosts',
+  through: 'PostLike'
+});
+
+//Domain
+db.User.hasMany(db.Domain);
+db.Domain.belongsTo(db.User);
+
+module.exports = db;
