84d246b7331e9273f90e90a721221fb83c8debc0
ycc4477 <ycc4477@gmail.com>
2020-06-25 00:33:56 +0900

    10.6

--- cyc/Chapter10/nodebird-api/routes/middlewares.js
+++ cyc/Chapter10/nodebird-api/routes/middlewares.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-
+const RateLimit = require('express-rate-limit');
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
@@ -21,7 +21,7 @@ exports.verifyToken = (req, res, next) => {
     req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
     return next();
   } catch (error) {
-    if(error.name === 'TokenExpiredError') {
+    if (error.name === 'TokenExpiredError') {
       return res.status(419).json({
         code: 419,
         message: '토큰이 만료되었습니다.'
@@ -34,3 +34,22 @@ exports.verifyToken = (req, res, next) => {
     })
   }
 }
+
+exports.apiLimiter = new RateLimit({
+  windowMs: 60 * 1000,
+  max: 1,
+  delayMs: 0,
+  handler(req, res) {
+    res.status(this.statusCode).json({
+      code: this.statusCode,
+      message: '1분에 한 번만 요청할 수 있습니다.',
+    });
+  },
+});
+
+exports.deprecated = (req, res) => {
+  res.status(410).json({
+    code: 410,
+    message: '새로운 버전이 나왔습니다. 새로운 버전을 사용하세요.',
+  });
+};
